5cdef376a93b66ab5841bf1fc9677c28434a71db
Parth Patil <parthpdevx@gmail.com>
2026-08-03 12:08:37 +0530

    Time: 0 ms (100.00%) | Space: 8.6 MB (20.65%) - 0070-climbing-stairs

--- 0070-climbing-stairs/solution.cpp
+++ 0070-climbing-stairs/solution.cpp
@@ -10,8 +10,17 @@ public:
         return memo[n];
     }
 
-    int climbStairs(int n) {
-        unordered_map<int, int> memo;
-        return climbStairs(n, memo);
+     int climbStairs(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        vector<int> dp(n+1);
+        dp[0] = dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
