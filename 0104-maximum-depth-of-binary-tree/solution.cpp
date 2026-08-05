d4c632966f6469ba806ae0042dc7e095ee5ab5cd
Parth Patil <parthpdevx@gmail.com>
2026-08-05 13:49:14 +0530

    Time: 3 ms (5.36%) | Space: 22.9 MB (16.15%) - 0104-maximum-depth-of-binary-tree

--- 0104-maximum-depth-of-binary-tree/solution.cpp
+++ 0104-maximum-depth-of-binary-tree/solution.cpp
@@ -12,14 +12,32 @@
 class Solution {
 public:
     int maxDepth(TreeNode* root) {
-        if(root == NULL){
-            return 0;
-        }
-        int depth = 0;
-        int left = maxDepth(root->left);
-        int right = maxDepth(root->right);
+        // if(root == NULL){
+        //     return 0;
+        // }
+        // int depth = 0;
+        // int left = maxDepth(root->left);
+        // int right = maxDepth(root->right);
 
-        depth = max(left,right) + 1;
-        return depth ;
+        // depth = max(left,right) + 1;
+        // return depth ;
+        stack<pair<TreeNode*,int>> st;
+        st.push({root,1});
+        int res = 0;
+
+        while(!st.empty()){
+            pair<TreeNode*, int> curr = st.top();
+            st.pop();
+            TreeNode* node = curr.first;
+            int depth = curr.second;
+
+            if(node!= NULL){
+                res = max(res,depth);
+                st.push({node->left , depth+1});
+                st.push({node->right, depth+1});
+            }
+        }
+
+        return res;
     }
 };
