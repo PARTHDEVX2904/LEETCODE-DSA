a409529d24ea348e37d08c03f2f0c75c4af76d53
Parth Patil <parthpdevx@gmail.com>
2026-08-05 16:52:19 +0530

    Time: 0 ms (100.00%) | Space: 34.5 MB (16.53%) - 0450-delete-node-in-a-bst

---
+++ 0450-delete-node-in-a-bst/solution.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(!root)
+            return NULL;
+        if(key>root->val){
+            root->right = deleteNode(root->right,key);
+        }else if(key<root->val){
+            root->left = deleteNode(root->left,key);
+        }else{
+            //single child probs
+            if(!root->right) return root->left;
+            if(!root->left) return root->right;
+
+            //two child probs - take the inorder successor
+            TreeNode* node = root->right;
+            while(node->left){
+                node = node->left;
+            }
+            root->val = node->val;
+            root->right = deleteNode(root->right,root->val);
+
+
+        }
+        return root;
+    }
+};
