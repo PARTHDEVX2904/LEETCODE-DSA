02384006c6452aa84495d0ca9c13752545a1134d
Parth Patil <parthpdevx@gmail.com>
2026-08-05 13:34:20 +0530

    Time: 0 ms (100.00%) | Space: 12.6 MB (26.68%) - 0226-invert-binary-tree

--- 0226-invert-binary-tree/solution.cpp
+++ 0226-invert-binary-tree/solution.cpp
@@ -12,16 +12,23 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if(!root) return NULL;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            TreeNode* front = q.front();
-            q.pop();
-            swap(front->left, front->right);
-            if(front->left) q.push(front->left);
-            if(front->right) q.push(front->right);
-        }
-        return root;
-    }
+    //     if(!root) return NULL;
+    //     queue<TreeNode*> q;
+    //     q.push(root);
+    //     while(!q.empty()){
+    //         TreeNode* front = q.front();
+    //         q.pop();
+    //         swap(front->left, front->right);
+    //         if(front->left) q.push(front->left);
+    //         if(front->right) q.push(front->right);
+    //     }
+    //     return root;
+            if(!root) return NULL;
+
+            swap(root->left, root->right);
+            invertTree(root->left);
+            invertTree(root->right);
+
+            return root;
+     }
 };
