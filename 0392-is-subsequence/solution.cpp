333f27e956b4732d328d077ea1897ba8c2a30d52
Parth Patil <parthpdevx@gmail.com>
2026-08-08 21:56:35 +0530

    Time: 4 ms (2.16%) | Space: 11.1 MB (5.22%) - 0392-is-subsequence

--- 0392-is-subsequence/solution.cpp
+++ 0392-is-subsequence/solution.cpp
@@ -1,16 +1,25 @@
 class Solution {
 public:
+    // bool isSubsequence(string s, string t) {
+    //     vector<int> dp()
+    //     return rec(s,t,0,0);
+    // }
     bool isSubsequence(string s, string t) {
-        return rec(s,t,0,0);
+        int n = s.size(), m = t.size();
+        vector<vector<int>> memo(n, vector<int>(m, -1));
+        return rec(s, t, 0, 0, memo);
     }
-    bool rec(string s , string t, int i,int j){
-        if(i == s.size()) return true;
-        if(j == t.size())  return false;
 
-        if(s[i]==t[j]){
-            return rec(s,t,i+1,j+1);
+private:
+    bool rec(string& s, string& t, int i, int j, vector<vector<int>>& memo) {
+        if (i == s.size()) return true;
+        if (j == t.size()) return false;
+        if (memo[i][j] != -1) return memo[i][j] == 1;
+        if (s[i] == t[j]) {
+            memo[i][j] = rec(s, t, i + 1, j + 1, memo) ? 1 : 0;
+        } else {
+            memo[i][j] = rec(s, t, i, j + 1, memo) ? 1 : 0;
         }
-
-        return rec(s,t,i,j+1);
+        return memo[i][j] == 1;
     }
 };
