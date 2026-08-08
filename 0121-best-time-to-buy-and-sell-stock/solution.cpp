0e04ba10a29c94ce677c158c46639569f638315f
Parth Patil <parthpdevx@gmail.com>
2026-08-09 00:28:12 +0530

    Time: 0 ms (100.00%) | Space: 97.5 MB (7.17%) - 0121-best-time-to-buy-and-sell-stock

---
+++ 0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minBuy = prices[0];
+        int n = prices.size();
+        int maxP = 0;
+        for(int &sell : prices ){
+            maxP = max(maxP, sell-minBuy);
+            minBuy = min(sell,minBuy);
+        }
+        return maxP;
+    }
+};
