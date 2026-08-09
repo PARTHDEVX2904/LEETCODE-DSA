a37490d82610b761951673d42fc846f8af2f92d2
Parth Patil <parthpdevx@gmail.com>
2026-08-09 16:14:10 +0530

    Time: 3 ms (31.38%) | Space: 11.2 MB (20.39%) - 0090-subsets-ii

--- 0090-subsets-ii/solution.cpp
+++ 0090-subsets-ii/solution.cpp
@@ -15,15 +15,39 @@ public:
         solve(nums,index+1,output,st,ans);
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        // vector<vector<int>> ans;
+        // set<vector<int>> st;
+        // vector<int> output;
+        // sort(nums.begin(), nums.end());
+        // int index = 0;
+        // solve(nums,index,output,st,ans);
+        
+        // for(auto v:st){
+        //     ans.push_back(v);
+        // }
+
+        // return ans;
         vector<vector<int>> ans;
         set<vector<int>> st;
-        vector<int> output;
+
         sort(nums.begin(), nums.end());
-        int index = 0;
-        solve(nums,index,output,st,ans);
-        
-        for(auto v:st){
-            ans.push_back(v);
+
+        int n = nums.size();
+
+        for (int mask = 0; mask < (1 << n); mask++) {
+            vector<int> temp;
+
+            for (int i = 0; i < n; i++) {
+                if (mask & (1 << i)) {
+                    temp.push_back(nums[i]);
+                }
+            }
+
+            st.insert(temp);
+        }
+
+        for (auto x : st) {
+            ans.push_back(x);
         }
 
         return ans;
