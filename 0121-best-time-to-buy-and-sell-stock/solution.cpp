5817c037daf5166ca3f54877dfc48e70729a92ab
Parth Patil <parthpdevx@gmail.com>
2026-08-09 07:07:44 +0530

    Time: 0 ms (100.00%) | Space: 97.2 MB (88.12%) - 0121-best-time-to-buy-and-sell-stock

--- 0121-best-time-to-buy-and-sell-stock/solution.cpp
+++ 0121-best-time-to-buy-and-sell-stock/solution.cpp
@@ -1,12 +1,23 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minBuy = prices[0];
-        int n = prices.size();
+        // int minBuy = prices[0];
+        // int n = prices.size();
+        // int maxP = 0;
+        // for(int &sell : prices ){
+        //     maxP = max(maxP, sell-minBuy);
+        //     minBuy = min(sell,minBuy);
+        // }
+        // return maxP;
+        int buy = prices[0];
         int maxP = 0;
-        for(int &sell : prices ){
-            maxP = max(maxP, sell-minBuy);
-            minBuy = min(sell,minBuy);
+        for(int i=1;i<prices.size();i++){
+            if(prices[i] >= buy){
+                maxP = max(maxP,prices[i]-buy);
+            }
+            else{
+                buy = min(buy,prices[i]);
+            }
         }
         return maxP;
     }
