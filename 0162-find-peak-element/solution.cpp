f4dc8115e69d85f6036b2509f2d2608234a11fd9
Parth Patil <parthpdevx@gmail.com>
2026-08-12 01:13:14 +0530

    Time: 0 ms (100.00%) | Space: 12.7 MB (42.11%) - 0162-find-peak-element

---
+++ 0162-find-peak-element/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n = nums.size();
+        int ans = -1;
+        int s = 0;
+        int e = n-1;
+        int mid = s+(e-s)/2;
+        while(s<e){
+            if(nums[mid]<nums[mid+1]){
+                s = mid+1;
+            }
+            else{
+                
+                e = mid;
+                ans = mid;
+            }
+            mid = s+(e-s)/2;
+        }
+        return e;
+    }
+};
