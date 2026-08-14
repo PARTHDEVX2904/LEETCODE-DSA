4fbb158dbaf0515e96c207a9691c261a2d506b6b
Parth Patil <parthpdevx@gmail.com>
2026-08-14 22:33:26 +0530

    Time: 1 ms (85.06%) | Space: 17.1 MB (32.77%) - 0179-largest-number

---
+++ 0179-largest-number/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string largestNumber(vector<int>& nums) {
+        vector<string> arr;
+        for (int num : nums) {
+            arr.push_back(to_string(num));
+        }
+
+        sort(arr.begin(), arr.end(), [](string& a, string& b) {
+            return a + b > b + a;
+        });
+
+        string res;
+        for (string& num : arr) {
+            res += num;
+        }
+
+        return res[0] == '0' ? "0" : res;
+    }
+};
