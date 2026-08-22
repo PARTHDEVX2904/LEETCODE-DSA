ff87f5b5fb4605fb8615751fa416edd7f94f4292
Parth Patil <parthpdevx@gmail.com>
2026-08-22 10:01:26 +0530

    Time: 0 ms (100.00%) | Space: 13.2 MB (54.88%) - 0684-redundant-connection

--- 0684-redundant-connection/solution.cpp
+++ 0684-redundant-connection/solution.cpp
@@ -3,14 +3,14 @@ public:
     vector<int> parent;
     vector<int> rank;
 
-    int find(int i) {
+    int find(int i,vector<int> &parent) {
         if (parent[i] != i)
-            parent[i] = find(parent[i]); // Path compression
+            parent[i] = find(parent[i],parent); // Path compression
         return parent[i];
     }
 
-    void join(int u, int v) {
-        int rootU = find(u), rootV = find(v);
+    void join(int u, int v,vector<int> &parent,vector<int> &rank) {
+        int rootU = find(u,parent), rootV = find(v,parent);
         if (rootU != rootV) {
             // Union by rank to balance trees
             if (rank[rootU] > rank[rootV]) parent[rootV] = rootU;
@@ -23,16 +23,16 @@ public:
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n = edges.size();
-        parent.resize(n + 1);
-        rank.resize(n + 1, 0);
+        vector<int> parent(n+1);
+        vector<int> rank(n+1,0);
         
         // Initialize each node as its own parent
         for (int i = 1; i <= n; i++) parent[i] = i;
 
         for (auto& edge : edges) {
             int u = edge[0], v = edge[1];
-            if (find(u) == find(v)) return edge; // Cycle detected!
-            join(u, v); // Merge sets
+            if (find(u,parent) == find(v,parent)) return edge; // Cycle detected!
+            join(u, v,parent,rank); // Merge sets
         }
         
         return {};
