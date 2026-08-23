02882077721dea97bb9aca55e5b0951c36159a8e
Parth Patil <parthpdevx@gmail.com>
2026-08-23 11:44:49 +0530

    Time: 93 ms (60.40%) | Space: 242.1 MB (77.84%) - 4031-find-all-numbers-disappeared-in-an-array-ii

--- 4031-find-all-numbers-disappeared-in-an-array-ii/solution.cpp
+++ 4031-find-all-numbers-disappeared-in-an-array-ii/solution.cpp
@@ -1,42 +1,61 @@
 class Solution {
 public:
     vector<vector<int>> findDisappearedNumbers(vector<int>& nums, int lower, int upper) {
-        sort(nums.begin(),nums.end());
-        set<int> st;
-        for(int num : nums){
-            st.insert(num);
-        }
+        // sort(nums.begin(),nums.end());
+        // set<int> st;
+        // for(int num : nums){
+        //     st.insert(num);
+        // }
+        // vector<vector<int>> res;
+        // vector<int> temp;
+        // int i = lower;
+        // while(i <= upper){
+        //     if(st.count(i)==0){
+        //         if(temp.empty() || temp.size()==1){
+        //             temp.push_back(i);
+        //         }else{
+        //             temp.back() = i;
+        //         }
+        //     }
+        //     else{
+        //         if(!temp.empty()){
+        //         if(temp.size()==1){
+        //             res.push_back({temp[0],temp[0]});
+        //         }else{
+        //             res.push_back(temp);
+        //         }
+        //         temp.clear();
+        //        }
+        //     }
+        //     i++;
+        // }
+        //     if(!temp.empty()){
+        //             if(temp.size()==1){
+        //                 res.push_back({temp[0],temp[0]});
+        //             }else{
+        //                 res.push_back(temp);
+        //             }
+        //             temp.clear();
+        //         }
+        // return res;
+
+        sort(begin(nums), end(nums));
         vector<vector<int>> res;
-        vector<int> temp;
-        int i = lower;
-        while(i <= upper){
-            if(st.count(i)==0){
-                if(temp.empty() || temp.size()==1){
-                    temp.push_back(i);
-                }else{
-                    temp.back() = i;
-                }
-            }
-            else{
-                if(!temp.empty()){
-                if(temp.size()==1){
-                    res.push_back({temp[0],temp[0]});
-                }else{
-                    res.push_back(temp);
-                }
-                temp.clear();
-               }
-            }
-            i++;
+
+        int st = lower;
+        for(int i = 0; i < nums.size(); ++i) {
+            if(nums[i] < lower || nums[i] > upper) 
+                continue;
+
+            if(nums[i] > st)
+                res.push_back({st, nums[i] - 1});
+            
+            st = nums[i] + 1;
         }
-            if(!temp.empty()){
-                    if(temp.size()==1){
-                        res.push_back({temp[0],temp[0]});
-                    }else{
-                        res.push_back(temp);
-                    }
-                    temp.clear();
-                }
+
+        if(st <= upper)
+            res.push_back({st, upper});
+        
         return res;
     }
 };
