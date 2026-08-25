c3ca87f0f423436cda87565678a5dc9139b04a1a
Parth Patil <parthpdevx@gmail.com>
2026-08-25 11:29:30 +0530

    Time: 3 ms (62.72%) | Space: 14.2 MB (12.36%) - 0241-different-ways-to-add-parentheses

---
+++ 0241-different-ways-to-add-parentheses/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int perform(int x, int y, char op) {
+        if(op == '+') return x + y;
+        if(op == '-') return x - y;
+        if(op == '*') return x * y;
+        return 0;
+    }
+
+    vector<int> diffWaysToCompute(string expression) {
+        vector<int> res;
+        for (int i = 0; i < expression.size(); ++i) {
+            char oper = expression[i];
+            if (oper == '+' || oper == '-' || oper == '*') {
+                vector<int> s1 = diffWaysToCompute(expression.substr(0, i));
+                vector<int> s2 = diffWaysToCompute(expression.substr(i + 1));
+                for (int a : s1) {
+                    for (int b : s2) {
+                        if (oper == '+') res.push_back(a + b);
+                        else if (oper == '-') res.push_back(a - b);
+                        else if (oper == '*') res.push_back(a * b);
+                    }
+                }
+            }
+        }
+        if (res.empty()) res.push_back(stoi(expression));
+        return res;
+    }
+};
