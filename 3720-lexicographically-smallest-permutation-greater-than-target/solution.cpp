489366bb7f99259d14c2fa86c67f9ad458eb799e
Parth Patil <parthpdevx@gmail.com>
2026-08-28 19:37:41 +0530

    Time: 4 ms (58.33%) | Space: 11.4 MB (42.26%) - 3720-lexicographically-smallest-permutation-greater-than-target

--- 3720-lexicographically-smallest-permutation-greater-than-target/solution.cpp
+++ 3720-lexicographically-smallest-permutation-greater-than-target/solution.cpp
@@ -17,21 +17,18 @@ public:
     string lexGreaterPermutation(string s, string target) {
         int n = s.size();
 
-        // Frequency of characters in s
+        
         vector<int> cnt(26, 0);
 
         for (char c : s) {
             cnt[c - 'a']++;
         }
 
-        // Try the position where we make the string greater.
-        // Rightmost position is preferred.
+        
         for (int i = n - 1; i >= 0; i--) {
 
-            // Rebuild the frequency array for this pivot.
+            
             vector<int> remain = cnt;
-
-            // Try to keep target[0 ... i-1] unchanged.
             bool possible = true;
 
             for (int j = 0; j < i; j++) {
@@ -48,8 +45,6 @@ public:
             if (!possible)
                 continue;
 
-            // At position i, we need the smallest
-            // available character strictly greater than target[i].
             int targetChar = target[i] - 'a';
 
             for (int c = targetChar + 1; c < 26; c++) {
@@ -59,12 +54,12 @@ public:
 
                 string ans = target.substr(0, i);
 
-                // Make the first difference here.
+                
                 ans += char('a' + c);
 
                 remain[c]--;
 
-                // Fill the rest in sorted order.
+                
                 for (int x = 0; x < 26; x++) {
                     ans.append(remain[x], char('a' + x));
                 }
