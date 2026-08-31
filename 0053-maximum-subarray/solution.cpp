2d49d48576dc3e8c83699d56888c3e0eb8dde625
Parth Patil <parthpdevx@gmail.com>
2026-08-31 21:55:28 +0530

    Time: 66 ms (4.51%) | Space: 71.9 MB (14.86%) - 0053-maximum-subarray

--- 0053-maximum-subarray/solution.cpp
+++ 0053-maximum-subarray/solution.cpp
@@ -14,20 +14,20 @@ public:
     //     }
 
     //     return res;  
-        // int currSum = nums[0];
-        // int maxSum = nums[0];
-        // for(int i=1;i<nums.size();i++){
-        //     currSum = max(nums[i],currSum+nums[i]);
-        //     maxSum = max(maxSum,currSum);
-        // }
-        // return maxSum;
-        int currSum = 0;
-        int maxSum = INT_MIN;
-        for(int i=0;i<nums.size();i++){
-            currSum += nums[i];
+        int currSum = nums[0];
+        int maxSum = nums[0];
+        for(int i=1;i<nums.size();i++){
+            currSum = max(nums[i],currSum+nums[i]);
             maxSum = max(maxSum,currSum);
-            if(currSum<0) currSum = 0;
         }
         return maxSum;
+        // int currSum = 0;
+        // int maxSum = INT_MIN;
+        // for(int i=0;i<nums.size();i++){
+        //     currSum += nums[i];
+        //     maxSum = max(maxSum,currSum);
+        //     if(currSum<0) currSum = 0;
+        // }
+        // return maxSum;
     }
 };
