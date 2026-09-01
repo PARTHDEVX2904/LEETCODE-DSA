c63aeab0fc179d1c36e6821c3b90ef195f6f47ca
Parth Patil <parthpdevx@gmail.com>
2026-09-01 05:48:27 +0530

    Time: 0 ms (100.00%) | Space: 112.7 MB (8.51%) - 0134-gas-station

--- 0134-gas-station/solution.cpp
+++ 0134-gas-station/solution.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-       int n = gas.size();
-        int start = n - 1, end = 0;
-        int tank = gas[start] - cost[start];
-        while (start > end) {
-            if (tank < 0) {
-                start--;
-                tank += gas[start] - cost[start];
-            } else {
-                tank += gas[end] - cost[end];
-                end++;
+        int n = gas.size();
+        int balance = 0;
+        int start = 0;
+        int deficit = 0;
+        for(int i=0;i<n;i++){
+            balance += gas[i]-cost[i];
+            if(balance < 0){
+                deficit += balance;
+                start = i+1;
+                balance = 0;
             }
+
         }
-        return tank >= 0 ? start : -1;
-    } 
+
+        if(deficit + balance >= 0){
+            return start;
+        }
+        else{
+            return -1;
+        }
+    }
 };
