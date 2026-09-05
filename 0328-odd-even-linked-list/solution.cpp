f9a48732faf4e110e9ac2556f3e9d9aee4d29ec6
Parth Patil <parthpdevx@gmail.com>
2026-09-05 12:09:14 +0530

    Time: 0 ms (100.00%) | Space: 15.5 MB (88.87%) - 0328-odd-even-linked-list

---
+++ 0328-odd-even-linked-list/solution.cpp
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* oddEvenList(ListNode* head) {
+        if(head == NULL || head->next == NULL)
+            return head;
+
+        ListNode* dummOdd = new ListNode(-1);
+        ListNode* dummEven = new ListNode(-1);
+
+        ListNode* tailOdd = dummOdd;
+        ListNode* tailEven = dummEven;
+
+        ListNode* curr = head;
+
+        int pos = 1;
+
+        while(curr != NULL) {
+
+            if(pos % 2 == 1) {
+                // Odd position
+
+                tailOdd->next = curr;
+                tailOdd = tailOdd->next;
+            }
+            else {
+                // Even position
+
+                tailEven->next = curr;
+                tailEven = tailEven->next;
+            }
+
+            curr = curr->next;
+            pos++;
+        }
+
+        // Important: terminate even list
+        tailEven->next = NULL;
+
+        // Connect odd list to even list
+        tailOdd->next = dummEven->next;
+
+        return dummOdd->next;
+    }
+};
