d614b924bc12480dcff27b0f4a2e441a97823a0e
Parth Patil <parthpdevx@gmail.com>
2026-09-08 16:57:05 +0530

    Time: 0 ms (100.00%) | Space: 8.7 MB (20.49%) - 0070-climbing-stairs

--- 0070-climbing-stairs/solution.cpp
+++ 0070-climbing-stairs/solution.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
-     int climbStairs(int n, unordered_map<int, int>& memo) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        if (memo.find(n) == memo.end()) {
-            memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo);
-        }
-        return memo[n];
+    int solve(int i,vector<int> &dp){
+        if(i==0) return 1;
+        if(i==1) return 1;
+
+        if(dp[i]!= -1) return dp[i];
+
+        int left = solve(i-1,dp);
+        int right = solve(i-2,dp);
+
+        return dp[i]=left + right;
     }
-
-     int climbStairs(int n) {
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-
-        vector<int> dp(n+1);
-        dp[0] = dp[1] = 1;
-        
-        for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-        return dp[n];
+    int climbStairs(int n) {
+        vector<int> dp(n+1,-1);
+       return solve(n,dp);
     }
 };
